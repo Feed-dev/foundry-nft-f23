186fdf88c3636dd47890ee5ea7f8ef4338ad2afd
Feed-dev <federico.lubbert@gmail.com>
2023-10-09 22:04:16 +0200

    Lesson 11: ERC721 mint function

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -5,5 +5,11 @@ pragma solidity ^0.8.18;
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BasicNft is ERC721 {
-    constructor() ERC721("Frogie", "Frog") {}
+    uint256 private s_tokencounter;
+
+    constructor() ERC721("Frogie", "Frog") {
+        s_tokencounter = 0;
+    }
+
+    function mintNFT() public {}
 }
