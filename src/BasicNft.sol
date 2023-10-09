f64d95ed0ef52e0046d3548ddeb147e602a03c0d
Feed-dev <federico.lubbert@gmail.com>
2023-10-09 22:00:33 +0200

    Lesson 11: ERC721 constructor

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -4,4 +4,6 @@ pragma solidity ^0.8.18;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract BasicNft {}
+contract BasicNft is ERC721 {
+    constructor() ERC721("Frogie", "Frog") {}
+}
