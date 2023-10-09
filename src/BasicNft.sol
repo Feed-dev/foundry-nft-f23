1fac6597b0c48b2495e2d354feaddbf2f83f0667
Feed-dev <federico.lubbert@gmail.com>
2023-10-09 21:58:01 +0200

    Lesson 11: ERC721

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.18;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BasicNft {}
