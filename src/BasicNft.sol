51a412ff00a27b7a3bc214d07d47f9d9917bcd23
Feed-dev <federico.lubbert@gmail.com>
2023-10-09 22:11:11 +0200

    Lesson 11: NFT Images

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -7,7 +7,7 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract BasicNft is ERC721 {
     uint256 private s_tokencounter;
 
-    constructor() ERC721("Frogie", "Frog") {
+    constructor() ERC721("Dogie", "Dog") {
         s_tokencounter = 0;
     }
 
