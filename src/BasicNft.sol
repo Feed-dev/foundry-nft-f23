95c0da0246bc2d0a070296b0adf63a0cd8b38246
Feed-dev <federico.lubbert@gmail.com>
2023-10-10 19:36:43 +0200

    Lesson 11: ERC721 token Uri

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -5,17 +5,23 @@ pragma solidity ^0.8.18;
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BasicNft is ERC721 {
-    uint256 private s_tokencounter;
+    uint256 private s_tokenCounter;
+    mapping(uint256 => string) private s_tokenIdToUri;
 
     constructor() ERC721("Dogie", "Dog") {
-        s_tokencounter = 0;
+        s_tokenCounter = 0;
     }
 
-    function mintNFT() public {}
+    function mintNFT(string memory tokenUri) public {
+        s_tokenIdToUri[s_tokenCounter] = tokenUri;
+
+        s_tokenCounter++;
+    }
 
     function tokenURI(
         uint256 tokenId
     ) public view override returns (string memory) {
-        return "";
+        return
+            "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
     }
 }
