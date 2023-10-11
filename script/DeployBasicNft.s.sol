fad50c702f036b3b9bd7d852a9bccb019aa9eb3a
Feed-dev <federico.lubbert@gmail.com>
2023-10-11 23:04:10 +0200

    Lesson 11: Deploy Basic NFT

--- script/DeployBasicNft.s.sol
+++ script/DeployBasicNft.s.sol
@@ -1,3 +1,8 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {BasicNft} from "../src/BasicNft.sol";
+
+contract DeployBasicNft is Script {}
