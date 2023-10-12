d1c5d481fb9d00ff6ad4e3147e126b820f1fdaa2
Feed-dev <federico.lubbert@gmail.com>
2023-10-12 21:36:02 +0200

    Lesson 11: Test basic nft

--- test/BasicNftTest.t.sol
+++ test/BasicNftTest.t.sol
@@ -4,5 +4,11 @@ pragma solidity ^0.8.19;
 
 import {BasicNft} from "../src/BasicNft.sol";
 import {Test, console} from "forge-std/Test.sol";
+import {DeployBasicNft} from "../script/DeployBasicNft.s.sol";
 
-contract BasicNftTest is Test {}
+contract BasicNftTest is Test {
+    BasicNft public basicNft;
+    DeployBasicNft public deployer;
+
+    function setUp() public {}
+}
