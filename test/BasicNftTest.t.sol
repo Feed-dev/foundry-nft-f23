fe9506be6048ad6a6904f26310d654ac75874956
Feed-dev <federico.lubbert@gmail.com>
2023-10-13 23:39:01 +0200

    Lesson 11: Test for basicNFT

--- test/BasicNftTest.t.sol
+++ test/BasicNftTest.t.sol
@@ -7,11 +7,24 @@ import {Test, console} from "forge-std/Test.sol";
 import {DeployBasicNft} from "../script/DeployBasicNft.s.sol";
 
 contract BasicNftTest is Test {
+    string constant NFT_NAME = "Dogie";
+    string constant NFT_SYMBOL = "DOG";
     BasicNft public basicNft;
     DeployBasicNft public deployer;
 
     function setUp() public {
         deployer = new DeployBasicNft();
         basicNft = deployer.run();
+    }
+
+    function testInitializedCorrectly() public view {
+        assert(
+            keccak256(abi.encodePacked(basicNft.name())) ==
+                keccak256(abi.encodePacked((NFT_NAME)))
+        );
+        assert(
+            keccak256(abi.encodePacked(basicNft.symbol())) ==
+                keccak256(abi.encodePacked((NFT_SYMBOL)))
+        );
     }
 }
