d976e5f0ead1c570bf94ad81755814a2a53b7c0a
Feed-dev <federico.lubbert@gmail.com>
2023-10-20 22:56:34 +0200

    Lesson 11: Interactions Script

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -3,7 +3,14 @@
 pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
+import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 
 contract MintBasicNft is Script {
-    function run() external {}
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "BasicNft",
+            block.chainid
+        );
+        mintNftOnContract();
+    }
 }
