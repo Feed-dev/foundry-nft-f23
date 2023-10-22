81a2166c14c6bd73856e3d854efaccfd97d2485b
Feed-dev <federico.lubbert@gmail.com>
2023-10-22 22:52:54 +0200

    Lesson 11: Script

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -4,8 +4,12 @@ pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
 import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
+import {BasicNft} from "../src/BasicNft.sol";
 
 contract MintBasicNft is Script {
+    string public constant PUG =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
     function run() external {
         address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
             "BasicNft",
@@ -14,5 +18,9 @@ contract MintBasicNft is Script {
         mintNftOnContract(mostRecentlyDeployed);
     }
 
-    function mintNftOnContract(address contractAddress) public {}
+    function mintNftOnContract(address contractAddress) public {
+        vm.startBroadcast();
+        BasicNft(contractAddress).mintNft(PUG);
+        vm.stopBroadcast();
+    }
 }
